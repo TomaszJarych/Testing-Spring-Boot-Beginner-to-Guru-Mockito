7792eafdca14a85714441f53cee26854d80d7a4b
Tomasz Jarych <tjarych@gmail.com>
2018-11-17 17:47:21 +0100

    Returning values from Mockito Mockss

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,19 +1,27 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -63,6 +71,22 @@ class SpecialitySDJpaServiceTest {
 		service.delete(speciality);
 
 		verify(specialtyRepository).delete(speciality);
+	}
+
+	@Test
+	void findById() {
+		Speciality specialityStub = new Speciality();
+
+		when(specialtyRepository.findById(1L)).thenReturn(Optional.of(specialityStub));
+
+		Speciality actualSpecialty = service.findById(1L);
+
+		assertNotNull(actualSpecialty);
+		assertEquals(specialityStub, actualSpecialty);
+		verify(specialtyRepository).findById(1L);
+		
+		// AssertJ
+		assertThat(actualSpecialty).isNotNull().isEqualTo(specialityStub);
 	}
 
 }
