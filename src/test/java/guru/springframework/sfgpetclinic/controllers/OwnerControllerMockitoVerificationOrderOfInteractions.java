05f42613c9818bd1bd74a54d9ae21d945f7ea7e6
Tomasz Jarych <tjarych@gmail.com>
2019-05-06 15:31:08 +0200

    Mockito Order verification - inOrder()

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerMockitoVerificationOrderOfInteractions.java
@@ -0,0 +1,87 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.fauxspring.Model;
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.inOrder;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerMockitoVerificationOrderOfInteractions {
+    private static final String OWNERS_CREATE_OR_UPDATE_OWNER_FORM = "owners/createOrUpdateOwnerForm";
+    private static final String REDIRECT_OWNERS_5 = "redirect:/owners/5";
+
+    @Mock
+    private OwnerService ownerService;
+
+    @Mock
+    BindingResult bindingResult;
+
+    @Mock
+    Model model;
+
+    private OwnerController controller;
+
+    @Captor
+    ArgumentCaptor<String> annotatedArgumentCaptor;
+
+    @BeforeEach
+    void setUp() {
+        controller = new OwnerController(ownerService);
+
+        given(ownerService.findAllByLastNameLike(annotatedArgumentCaptor.capture())).willAnswer(invocation -> {
+            List<Owner> stubList = new ArrayList<>();
+            String name = invocation.getArgument(0);
+            if (name.equals("%Buck%")) {
+                stubList.add(new Owner(1L, "Joe", "Buck"));
+                return stubList;
+            } else if (name.equals("%DontFindMe%")) {
+                return stubList;
+            } else if (name.equals("%FindMe%")) {
+                stubList.add(new Owner(1L, "Joe", "FindMe"));
+                stubList.add(new Owner(2L, "John", "FindMe"));
+                return stubList;
+            } else {
+                throw new RuntimeException("Invalid argument has been passed");
+            }
+        });
+    }
+
+
+    @Test
+    @DisplayName("Order verification")
+    void orderVerification() {
+        //given
+        Owner owner = new Owner(1L, "Joe", "FindMe");
+        InOrder order = inOrder(ownerService, model);
+
+        //when
+        String viewName = controller.processFindForm(owner, bindingResult, model);
+
+        //then
+
+        assertThat("%FindMe%").isEqualToIgnoringCase(annotatedArgumentCaptor.getValue());
+        assertThat("owners/ownersList").isEqualToIgnoringCase(viewName);
+
+        //inOrderAssertions
+        order.verify(ownerService).findAllByLastNameLike(anyString());
+        order.verify(model).addAttribute(anyString(), anyList());
+    }
+}
